e8fd48a5a7b7c68c4e3501b17475867838c093a5
jjsemaan <jalal.semaan@parabim.com>
2024-04-07 11:10:58 +0000

    Add kitchen app to settings.py

--- nadasProject/settings.py
+++ nadasProject/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-#-e1%3q4q@nz&!eqynak7w6k4@-yv+h$r%mms=(e=tva2u3=qz'
+
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'customer',
+    'kitchen',
 ]
 
 MIDDLEWARE = [
