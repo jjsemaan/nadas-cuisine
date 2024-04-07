f59a253b1cef9df6d8dd678306e6dbb4bdedab55
jjsemaan <jalal.semaan@parabim.com>
2024-04-07 11:05:14 +0000

    Add secret key

--- nadasProject/settings.py
+++ nadasProject/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#-e1%3q4q@nz&!eqynak7w6k4@-yv+h$r%mms=(e=tva2u3=qz'
-
+#SECRET_KEY = 'django-insecure-#-e1%3q4q@nz&!eqynak7w6k4@-yv+h$r%mms=(e=tva2u3=qz'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
