b6fa98af89bddb9b778259ae7c48d5139572e118
jjsemaan <jalal.semaan@parabim.com>
2024-04-07 11:36:33 +0000

    Add whitenoise middleware

--- nadasProject/settings.py
+++ nadasProject/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-jjsemaan-nadas-cuisine-vm5266xi9n.us1.codeanyapp.com','.herokuapp.com']
 
@@ -45,8 +45,13 @@ INSTALLED_APPS = [
     'kitchen',
 ]
 
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -113,6 +118,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Email verification is disabled in this project.
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
