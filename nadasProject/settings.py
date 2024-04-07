d82af14897bcc69de3aa46941712f1076b1fb1e3
jjsemaan <jalal.semaan@parabim.com>
2024-04-07 11:34:07 +0000

    Add csrf trusted origin

--- nadasProject/settings.py
+++ nadasProject/settings.py
@@ -32,7 +32,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['8000-jjsemaan-nadas-cuisine-vm5266xi9n.us1.codeanyapp.com','.herokuapp.com']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -90,6 +89,11 @@ WSGI_APPLICATION = 'nadasProject.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
