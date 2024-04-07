dd1d6c7f3878a0175118565f9ed978975648fe64
jjsemaan <jalal.semaan@parabim.com>
2024-04-07 11:18:34 +0000

    Install whitenoise

--- nadasProject/settings.py
+++ nadasProject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-jjsemaan-nadas-cuisine-vm5266xi9n.us1.codeanyapp.com', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-jjsemaan-nadas-cuisine-vm5266xi9n.us1.codeanyapp.com','.herokuapp.com']
 
 
 # Application definition
