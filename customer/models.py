dd46a69eef93ce4b3e9ca138e3b1ae528fa81430
jjsemaan <jalal.semaan@parabim.com>
2024-04-14 00:51:16 +0000

    Add customer id to order model

--- customer/models.py
+++ customer/models.py
@@ -52,7 +52,7 @@ class OrderModel(models.Model):
     """
     Represents an order made by a customer, including its items and total price.
     """
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user_id = models.IntegerField()  # Storing user ID directly
     created_on = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     items = models.ManyToManyField(
